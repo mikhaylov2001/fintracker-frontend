7fc684fcd4582e9abb92db6a28b33a9f94f7bb39
dimon9995 <mikhailov1500@mail.ru>
2026-02-08 17:50:57 +0300

    hard word

--- src/services/googleAuthService.js
+++ src/services/googleAuthService.js
@@ -1,4 +1,5 @@
-// src/services/googleAuthService.js
-const GOOGLE_CLIENT_ID =
-  '1096583300191-ecs88krahb9drbhbs873ma4mieb7lihj.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
+if (!GOOGLE_CLIENT_ID) {
+  throw new Error('REACT_APP_GOOGLE_CLIENT_ID is not set');
+}
