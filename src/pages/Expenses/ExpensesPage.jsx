095e6d67a138a586ebed92ee1a4a7140bfb659a6
dimon9995 <mikhailov1500@mail.ru>
2026-02-20 20:18:18 +0300

    updated pages

--- src/pages/Expenses/ExpensesPage.jsx
+++ src/pages/Expenses/ExpensesPage.jsx
@@ -49,6 +49,7 @@ const CATEGORY_OPTIONS = [
   "Транспорт",
   "Коммунальные услуги",
   "Здоровье",
+  "Учеба",
   "Развлечения",
   "Другое",
 ];
@@ -485,24 +486,24 @@ export default function ExpensesPage() {
           onAction={openCreate}
         />
       ) : (
-        <Box sx={{ overflowX: "auto" }}>
+        <Box sx={{ overflowX: "hidden" }}>
           <Table
             size="small"
             sx={{
               width: "100%",
-              minWidth: { sm: 720 },
-              tableLayout: { xs: "fixed", sm: "auto" },
+              minWidth: { xs: "100%", sm: 720 },
+              tableLayout: { xs: "auto", sm: "auto" },
               bgcolor: "transparent",
               borderRadius: 0,
               overflow: "visible",
               border: "none",
               "& th, & td": {
                 px: { xs: 0.75, sm: 2 },
-                py: { xs: 1, sm: 1.1 },
+                py: { xs: 0.8, sm: 1.1 },
                 fontSize: { xs: 12, sm: 13 },
                 lineHeight: 1.3,
                 borderBottom: "none !important",
-                whiteSpace: "nowrap",
+                whiteSpace: { xs: "normal", sm: "nowrap" },
                 verticalAlign: "middle",
                 textAlign: "left",
               },
@@ -524,11 +525,24 @@ export default function ExpensesPage() {
           >
             <TableHead>
               <TableRow>
-                <TableCell sx={{ width: { xs: "20%", sm: 140 } }}>Дата</TableCell>
+                <TableCell sx={{ width: { xs: "22%", sm: 140 } }}>Дата</TableCell>
                 <TableCell sx={{ width: { xs: "28%", sm: 160 } }}>Сумма</TableCell>
-                <TableCell sx={{ width: { xs: "38%", sm: 200 } }}>Категория</TableCell>
-                <TableCell sx={{ width: 200, display: { xs: "none", sm: "table-cell" } }}>Описание</TableCell>
-                <TableCell sx={{ width: { xs: "14%", sm: 120 } }}>Действия</TableCell>
+                <TableCell sx={{ width: { xs: "35%", sm: 200 } }}>Категория</TableCell>
+                <TableCell
+                  sx={{
+                    width: 200,
+                    display: { xs: "none", sm: "table-cell" },
+                  }}
+                >
+                  Описание
+                </TableCell>
+                <TableCell
+                  sx={{
+                    width: { xs: "15%", sm: 120 },
+                  }}
+                >
+                  Действия
+                </TableCell>
               </TableRow>
             </TableHead>
 
@@ -537,18 +551,70 @@ export default function ExpensesPage() {
                 const dateLike = getExpenseDateLike(x);
                 return (
                   <TableRow key={x.id} hover>
-                    <TableCell>{isMobile ? formatDateRuShort(dateLike) : formatDateRu(dateLike)}</TableCell>
+                    <TableCell>
+                      {isMobile ? formatDateRuShort(dateLike) : formatDateRu(dateLike)}
+                    </TableCell>
 
-                    <TableCell sx={{ fontWeight: 900, color: COLORS.expenses }}>
+                    <TableCell
+                      sx={{
+                        fontWeight: 900,
+                        color: COLORS.expenses,
+                      }}
+                    >
                       {formatAmount(Number(x.amount || 0))}
                     </TableCell>
 
-                    <TableCell>{x.category}</TableCell>
+                    <TableCell sx={{ pr: { xs: 0.5, sm: 2 } }}>
+                      <Typography
+                        component="div"
+                        sx={{
+                          fontSize: { xs: 12, sm: 13 },
+                          fontWeight: 800,
+                          color: bankingColors.text,
+                          lineHeight: 1.15,
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          display: "-webkit-box",
+                          WebkitBoxOrient: "vertical",
+                          WebkitLineClamp: { xs: 2, sm: 1 },
+                        }}
+                        title={x.category || ""}
+                      >
+                        {x.category}
+                      </Typography>
 
-                    <TableCell sx={{ display: { xs: "none", sm: "table-cell" } }}>{x.description}</TableCell>
+                      {isMobile ? (
+                        <Typography
+                          component="div"
+                          sx={{
+                            mt: 0.2,
+                            fontSize: 11,
+                            color: bankingColors.muted,
+                            lineHeight: 1.15,
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap",
+                          }}
+                          title={x.description || ""}
+                        >
+                          {x.description}
+                        </Typography>
+                      ) : null}
+                    </TableCell>
 
-                    <TableCell>
-                      <IconButton onClick={() => openEdit(x)} size="small" sx={{ userSelect: "none" }}>
+                    <TableCell
+                      sx={{ display: { xs: "none", sm: "table-cell" } }}
+                      title={x.description || ""}
+                    >
+                      {x.description}
+                    </TableCell>
+
+                    <TableCell sx={{ textAlign: "center", whiteSpace: "nowrap" }}>
+                      <IconButton
+                        onClick={() => openEdit(x)}
+                        size="small"
+                        sx={{ userSelect: "none" }}
+                      >
                         <EditOutlinedIcon fontSize="small" sx={{ color: bankingColors.text }} />
                       </IconButton>
                       <IconButton
